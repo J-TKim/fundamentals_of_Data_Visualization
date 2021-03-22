b8143f6925b6784510302b506bfbde3f4630d59e
J-TKim <kkkd991020@naver.com>
2021-03-22 15:30:39 +0900

    Upload setting

---
+++ download_packages.R
@@ -0,0 +1,24 @@
+# devtools 패키지 설치
+install.packages("devtools")
+
+# CARN에 등재되지 않은 패키지 설치
+devtools::install_github("wilkelab/cowplot")
+install.packages("colorspace", dependencies = TRUE, INSTALL_opts = '--no-lock', repos = "http://R-Forge.R-project.org")
+devtools::install_github("clauswilke/colorblindr")
+devtools::install_github("clauswilke/dviz.supp")
+devtools::install_github("wilkelab/ungeviz")
+devtools::install_github("clauswilke/ggtextures")
+devtools::install_github("thomasp85/transformr")
+devtools::install_github("hrbrmstr/statebins")
+
+# CRAN에 등재된 패키지 설치
+install.packages(c(
+  "bookdown", "cowplot",
+  "tidyverse", "rgeos", "patchwork", "lwgeom",
+  "ggthemes", "ggforce", "ggmap", "geofacet",
+  "ggstance", "treemapify", "ggrepel",
+  "tidybayes", "ggstance", "plot3D", "tinter",
+  "ggridges", "emmeans", "mgcv", "sf", "showtext",
+  "magick", "nycflights13", "rwordlmap",
+  "gganimate"
+))
